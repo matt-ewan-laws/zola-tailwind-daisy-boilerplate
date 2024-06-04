64cd9dc6c0bcdb2479396be842d6ce23d785faea
Matthew Ewan-Laws <matthew.ewan-laws@newsquest.co.uk>
2024-06-04 11:29:09 +0100

    add markdown editor

--- src/main.js
+++ src/main.js
@@ -1,23 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const codeBlocks = document.querySelectorAll('pre code');
+import Editor from '@toast-ui/editor';
 
-  codeBlocks.forEach(function (codeBlock) {
-    const copyButton = document.createElement('button');
-    copyButton.innerHTML = 'Copy';
-    copyButton.className = 'copy-button';
-    codeBlock.parentNode.insertBefore(copyButton, codeBlock);
-
-    copyButton.addEventListener('click', function () {
-      const range = document.createRange();
-      range.selectNode(codeBlock);
-      window.getSelection().removeAllRanges();
-      window.getSelection().addRange(range);
-      document.execCommand('copy');
-      window.getSelection().removeAllRanges();
-      copyButton.innerHTML = 'Copied!';
-      setTimeout(function () {
-        copyButton.innerHTML = 'Copy';
-      }, 2000);
-    });
+document.addEventListener('DOMContentLoaded', function() {
+  const editor = new Editor({
+    el: document.querySelector('#editor'),
+    height: '500px',
+    initialEditType: 'wysiwyg',
+    previewStyle: 'vertical'
   });
-});
+
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{css|js}', './templates/**/*.html', './src/main.css'],
+  content: ['./src/**/*.js', '.src/**/*.css', './templates/**/*.html', './src/main.css'],
   theme: {
     extend: {},
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
-    themes: ["retro", "cupcake", "dark"]
+    themes: ["winter", "night"]
   }
 }
 
